1808409bde393096c2d2748a7c46a3e91dff0561
Garima-8604 <garima.student.cse21@itbhu.ac.in>
2022-11-15 13:46:34 +0530

    Login done

---
+++ store/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+from store.view2 import authview
+
+urlpatterns = [
+    path('', views.home, name = 'home'),
+    path('categories', views.categories, name = 'categories'),
+    path('categories/<str:slug>', views.categoriesview, name = 'categoriesview'),
+    path('categories/<str:cate_slug>/<str:prod_slug>', views.productview, name="productview"),
+
+    path('register/', authview.register, name="register"),
+    path('login/', authview.loginpage, name="loginpage"),
+    path('logout/', authview.logoutpage, name="logoutpage"),
+
+]
